85bd489b76d887c7e528900f0a4f34db5dbba2e3
Gordon Plumb <gplumb98@gmail.com>
2020-11-17 21:06:12 -0500

    initial commit

---
+++ src/main/java/com/gordonplumb/matchhistory/RevampedMatchHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.gordonplumb.matchhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevampedMatchHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevampedMatchHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gordonplumb/matchhistory/RevampedMatchHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gordonplumb.matchhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevampedMatchHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
